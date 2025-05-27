9416ce2f019886b8feb3960a4482198ee9325764
sergey <molchanov.top@gmail.com>
2025-05-27 15:34:44 +0300

    refactoring: wb api

--- pkg/api/wb/api.go
+++ pkg/api/wb/api.go
@@ -12,24 +12,24 @@ import (
 	"time"
 )
 
-func stocksFbo(apiKey string) (string, error) {
-
-	url := "https://statistics-api.wildberries.ru/api/v1/supplier/stocks"
-	body := []byte(``)
-
-	req, err := http.NewRequest("GET", url, bytes.NewBuffer(body))
+func request(reqType, url string, headers map[string]string, params map[string]string, body []byte) (string, error) {
+	req, err := http.NewRequest(reqType, url, bytes.NewBuffer(body))
 
 	if err != nil {
 		log.Fatalf("Ошибка создания запроса: %v", err)
 	}
 
-	// Устанавливаем необходимые заголовки (если нужны)
-	req.Header.Set("Authorization", apiKey)
+	for s := range headers {
+		req.Header.Set(s, headers[s])
+	}
+
 	q := req.URL.Query()
-	q.Add("dateFrom", "2019-06-20")
+	for s := range params {
+		q.Add(s, params[s])
+	}
+
 	req.URL.RawQuery = q.Encode()
 
-	// Выполняем запрос
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -37,52 +37,69 @@ func stocksFbo(apiKey string) (string, error) {
 	}
 	defer resp.Body.Close()
 
-	// Проверяем статус ответа
 	if resp.StatusCode != http.StatusOK {
 		return "", fmt.Errorf("Ошибка получения ФБО остатков: получен статус %v", resp.Status)
 	}
 
-	// Читаем тело ответа
 	jsonString, _ := io.ReadAll(resp.Body)
-
-	// Выводим ответ
 	return string(jsonString), nil
 }
 
-func getOrdersBySupplyId(wildberriesKey, supplyId string) (string, error) {
-
-	url := "https://marketplace-api.wildberries.ru/api/v3/supplies/" + supplyId + "/orders"
-	req, err := http.NewRequest("GET", url, nil)
-
+func GET(url string, headers map[string]string, params map[string]string, body []byte) (string, error) {
+	response, err := request("GET", url, headers, params, body)
 	if err != nil {
-		return "", fmt.Errorf("ошибка создания запроса: %v", err)
+		return "", err
 	}
-
-	// Устанавливаем необходимые заголовки (если нужны)
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", wildberriesKey) // Замените на ваш API токен, если нужен
-
-	// Выполняем запрос
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", fmt.Errorf("ошибка выполнения запроса getOrdersBySupplyId: %v", err)
-	}
-	defer resp.Body.Close()
-
-	// Проверяем статус ответа
-	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("ошибка getOrdersBySupplyId: получен статус %v", resp.StatusCode)
-	}
-
-	// Читаем тело ответа
-	jsonString, _ := io.ReadAll(resp.Body)
-
-	// Выводим ответ
-	return string(jsonString), nil
+	return response, nil
 }
 
-func getCodesByOrderId(wildberriesKey string, orderId int) string {
+func POST(url string, headers map[string]string, params map[string]string, body []byte) (string, error) {
+	response, err := request("POST", url, headers, params, body)
+	if err != nil {
+		return "", err
+	}
+	return response, nil
+}
+
+func stocksFbo(token string) (string, error) {
+	baseURL := "https://statistics-api.wildberries.ru/api/v1/supplier/stocks"
+
+	body := []byte(``)
+
+	headers := map[string]string{
+		"Authorization": token,
+	}
+
+	params := map[string]string{
+		"dateFrom": "2019-06-20",
+	}
+
+	response, err := GET(baseURL, headers, params, body)
+	if err != nil {
+		return "", err
+	}
+
+	return response, nil
+}
+
+func getOrdersBySupplyId(token, supplyId string) (string, error) {
+
+	baseURL := "https://marketplace-api.wildberries.ru/api/v3/supplies/" + supplyId + "/orders"
+	body := []byte(``)
+
+	headers := map[string]string{
+		"Authorization": token,
+	}
+
+	response, err := GET(baseURL, headers, make(map[string]string), body)
+	if err != nil {
+		return "", err
+	}
+
+	return response, nil
+}
+
+func getCodesByOrderId(token string, orderId int) (string, error) {
 
 	params := url.Values{}
 	params.Add("type", "png")
@@ -103,86 +120,50 @@ func getCodesByOrderId(wildberriesKey string, orderId int) string {
 		Orders: []int{orderId},
 	}
 
-	// Преобразование данных в JSON
-	jsonData, err := json.Marshal(data)
+	body, err := json.Marshal(data)
 	if err != nil {
 		fmt.Println("Ошибка при преобразовании данных в JSON:", err)
 	}
 
-	req, err := http.NewRequest("POST", fullURL, bytes.NewBuffer(jsonData))
+	headers := map[string]string{
+		"Content-Type":  "application/json",
+		"Authorization": token,
+	}
 
+	response, err := POST(fullURL, headers, make(map[string]string), body)
 	if err != nil {
-		log.Fatalf("Ошибка создания запроса: %v", err)
+		return "", err
 	}
 
-	// Устанавливаем необходимые заголовки (если нужны)
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", wildberriesKey) // Замените на ваш API токен, если нужен
-
-	// Выполняем запрос
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("Ошибка выполнения запроса getCodesByOrderId: %v", err)
-	}
-	defer resp.Body.Close()
-
-	// Проверяем статус ответа
-	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("Ошибка getCodesByOrderId: получен статус %d", resp.StatusCode)
-	}
-
-	// Читаем тело ответа
-	jsonString, _ := io.ReadAll(resp.Body)
-
-	// Выводим ответ
-	return string(jsonString)
+	return response, nil
 }
 
-/*
-API метод для получения фбс заказов
-*/
-func ordersFBS(apiKey string, daysAgo int) string {
+// Получения фбс заказов
+func ordersFBS(token string, daysAgo int) (string, error) {
 
-	url := "https://marketplace-api.wildberries.ru/api/v3/orders"
+	baseURL := "https://marketplace-api.wildberries.ru/api/v3/orders"
 	body := []byte(``)
 
-	req, err := http.NewRequest("GET", url, bytes.NewBuffer(body))
+	headers := map[string]string{
+		"Authorization": token,
+	}
 
+	params := map[string]string{
+		"limit":    "1000",
+		"next":     "0",
+		"dateFrom": strconv.Itoa(int(getUnix(time.Now().AddDate(0, 0, -(daysAgo + 1))))),
+		"dateTo":   strconv.Itoa(int(getUnix(time.Now().AddDate(0, 0, -daysAgo)))),
+	}
+
+	response, err := GET(baseURL, headers, params, body)
 	if err != nil {
-		log.Fatalf("Ошибка создания запроса: %v", err)
+		return "", err
 	}
 
-	// Устанавливаем необходимые заголовки (если нужны)
-	req.Header.Set("Authorization", apiKey)
-	q := req.URL.Query()
-	q.Add("limit", "1000")
-	q.Add("next", "0")
-	q.Add("dateFrom", strconv.Itoa(int(getUnix(time.Now().AddDate(0, 0, -(daysAgo+1))))))
-	q.Add("dateTo", strconv.Itoa(int(getUnix(time.Now().AddDate(0, 0, -daysAgo)))))
-	req.URL.RawQuery = q.Encode()
-
-	// Выполняем запрос
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("Ошибка выполнения запроса: %v", err)
-	}
-	defer resp.Body.Close()
-
-	// Проверяем статус ответа
-	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("Ошибка получения ФБС заказов: получен статус %s", resp.Status)
-	}
-
-	// Читаем тело ответа
-	jsonString, _ := io.ReadAll(resp.Body)
-
-	// Выводим ответ
-	return string(jsonString)
+	return response, nil
 }
 
-func ordersFBSStatus(wildberriesKey string, orderId int) string {
+func ordersFBSStatus(token string, orderId int) (string, error) {
 
 	baseURL := "https://marketplace-api.wildberries.ru/api/v3/orders/status"
 
@@ -195,124 +176,75 @@ func ordersFBSStatus(wildberriesKey string, orderId int) string {
 	}
 
 	// Преобразование данных в JSON
-	jsonData, err := json.Marshal(data)
+	body, err := json.Marshal(data)
 	if err != nil {
 		fmt.Println("Ошибка при преобразовании данных в JSON:", err)
 	}
 
-	req, err := http.NewRequest("POST", baseURL, bytes.NewBuffer(jsonData))
+	headers := map[string]string{
+		"Content-Type":  "application/json",
+		"Authorization": token,
+	}
 
+	response, err := POST(baseURL, headers, make(map[string]string), body)
 	if err != nil {
-		log.Fatalf("Ошибка создания запроса: %v", err)
+		return "", err
 	}
 
-	// Устанавливаем необходимые заголовки (если нужны)
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", wildberriesKey) // Замените на ваш API токен, если нужен
-
-	// Выполняем запрос
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("Ошибка выполнения запроса: %v", err)
-	}
-	defer resp.Body.Close()
-
-	// Проверяем статус ответа
-	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("Ошибка: получен статус %d", resp.StatusCode)
-	}
-
-	// Читаем тело ответа
-	jsonString, _ := io.ReadAll(resp.Body)
-
-	// Выводим ответ
-	return string(jsonString)
+	return response, nil
 }
 
 /*
 API метод для получения всех заказов за дату, равную (now() - daysAgo). Максимум 1 запрос в минуту
 */
-func apiOrdersALL(apiKey string, daysAgo, flag int) string {
+func apiOrdersALL(token string, daysAgo, flag int) (string, error) {
 
 	date := time.Now().AddDate(0, 0, -daysAgo)
 
-	url := "https://statistics-api.wildberries.ru/api/v1/supplier/orders"
-	body := []byte(`{}`)
+	baseURL := "https://statistics-api.wildberries.ru/api/v1/supplier/orders"
 
-	req, err := http.NewRequest("GET", url, bytes.NewBuffer(body))
+	body := []byte(``)
 
+	headers := map[string]string{
+		"Authorization": token,
+	}
+
+	params := map[string]string{
+		"dateFrom": date.Format("2006-01-02"),
+		"flag":     strconv.Itoa(flag),
+	}
+
+	response, err := GET(baseURL, headers, params, body)
 	if err != nil {
-		log.Fatalf("Ошибка создания запроса: %v", err)
+		return "", err
 	}
 
-	// Устанавливаем необходимые заголовки (если нужны)
-	req.Header.Set("Authorization", apiKey)
-
-	q := req.URL.Query()
-	q.Add("dateFrom", date.Format("2006-01-02"))
-	q.Add("flag", strconv.Itoa(flag))
-	req.URL.RawQuery = q.Encode()
-
-	// Выполняем запрос
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("Ошибка выполнения запроса: %v", err)
-	}
-	defer resp.Body.Close()
-
-	// Проверяем статус ответа
-	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("Ошибка получения ALL заказов: получен статус %s", resp.Status)
-	}
-
-	// Читаем тело ответа
-	jsonString, _ := io.ReadAll(resp.Body)
-
-	// Выводим ответ
-	return string(jsonString)
+	return response, nil
 }
 
-func apiSalesAndReturns(apiKey string, daysAgo int) string {
+func apiSalesAndReturns(token string, daysAgo int) (string, error) {
 
 	date := time.Now().AddDate(0, 0, -daysAgo)
 
-	url := "https://statistics-api.wildberries.ru/api/v1/supplier/sales"
-	body := []byte(`{}`)
+	baseURL := "https://statistics-api.wildberries.ru/api/v1/supplier/sales"
 
-	req, err := http.NewRequest("GET", url, bytes.NewBuffer(body))
+	body := []byte(``)
 
+	headers := map[string]string{
+		"Authorization": token,
+	}
+
+	params := map[string]string{
+		"dateFrom": date.Format("2006-01-02"),
+		"flag":     "1",
+	}
+
+	response, err := GET(baseURL, headers, params, body)
 	if err != nil {
-		log.Fatalf("Ошибка создания запроса: %v", err)
+		return "", err
 	}
 
-	// Устанавливаем необходимые заголовки (если нужны)
-	req.Header.Set("Authorization", apiKey)
-
-	q := req.URL.Query()
-	q.Add("dateFrom", date.Format("2006-01-02"))
-	q.Add("flag", "1")
-	req.URL.RawQuery = q.Encode()
-
-	// Выполняем запрос
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatalf("Ошибка выполнения запроса: %v", err)
-	}
-	defer resp.Body.Close()
-
-	// Проверяем статус ответа
-	if resp.StatusCode != http.StatusOK {
-		log.Fatalf("Ошибка получения ALL заказов: получен статус %s", resp.Status)
-	}
-
-	// Читаем тело ответа
-	jsonString, _ := io.ReadAll(resp.Body)
-
-	// Выводим ответ
-	return string(jsonString)
+	return response, nil
 }
 
 func getUnix(date time.Time) int64 {
